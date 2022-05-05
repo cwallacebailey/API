fae17383d612283367ae4d07f8451ec3062e82d0
cwallacebailey <cwallacebailey@gmail.com>
2022-05-05 13:07:39 +0000

    final video done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -12,7 +13,52 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data.expiry);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Status"
+    document.getElementById("results-content").innerHTML = `<div>Your Key is valid until ${data.expiry}</div>`
+    resultsModal.show();
+}
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                                 },
+                        body: form
+    })
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayErrors() {
+    let heading = `JSHint Results for ${data.file}`;
+
+    if(data.total_errors === 0) {
+        results = `<div>No errors reported!</div>`
+    } else {
+        results = `<div>Total Errors: <span>${data.total_error}</span>`
+        for (let error of data.errors_list) {
+            results += `<div>At line <span>${error.line}</span>`;
+            results += `column <span>${error.col}</span></div>`;
+            results += `<div>${error.error}</div>`;
+        }
+    }
+    document.getElementById("resultsModalTitle").innerText = heading
+    document.getElementById("results-content").innerHTML = results
+    resultsModal.show();
 }
 
