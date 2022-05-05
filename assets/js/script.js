05f2a1351131349bc8a063d13de02a15083a3793
cwallacebailey <cwallacebailey@gmail.com>
2022-05-05 12:37:47 +0000

    first videos complete

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const API_KEY = "Ptiw52dqDsW3KS0Mj1N9_V8whfI";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
+
